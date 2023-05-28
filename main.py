919187727b835523859a45ebe64b736cf72de861
hannan680 <69585294+hannan680@users.noreply.github.com>
2023-05-27 19:26:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MODEL = tf.keras.models.load_model("./model/CropSense.h5")
+MODEL = tf.keras.models.load_model("./models/CropSense.h5")
 
 CLASS_NAMES = ['Early_Blight_Potato',
  'Healthy_Potato',
